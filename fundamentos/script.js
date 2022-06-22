56ddccf6d99f67b479a0882e8c11f9beb8808161
RogerPontes <benvindofuturo@gmail.com>
2022-06-22 19:00:51 -0300

    exercícios bl-4

---
+++ fundamentos/script.js
@@ -0,0 +1,30 @@
+let a = 2;
+let b = 2
+let resultado = a+b;
+console.log(resultado);
+
+let c = 4;
+let d = 2
+let result = a-b;
+console.log(resultado);
+
+let e = 6;
+let f = 2
+let resul = a/b;
+console.log(resultado);
+
+let g = 8;
+let h = 2
+let resu = a*b;
+console.log(resultado);
+
+let i = 4;
+let j = 4
+let resp = a%b;
+console.log(resultado);
+
+let numX = 12;
+let numZ = 15;
+let numeromaior = [numZ > numX];
+  console.log (numeromaior)
+ 
